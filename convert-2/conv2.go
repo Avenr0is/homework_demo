c2841e46d3e28f3fe126b8357a9099668b03adb4
Avenr0is <kimura.42@yandex.ru>
2026-04-02 13:59:37 +0700

    conv2

---
+++ convert-2/conv2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func userInput() string {
+
+	var inputnum string
+	fmt.Println("Введите число:")
+	fmt.Scan(&inputnum)
+
+	return inputnum
+}
+
+func calculate(value1 float64, value2 float64) (result float64) {
+
+	return
+}
+
+func main() {
+
+}
